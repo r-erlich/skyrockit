aaec9543b8ad281146ef4e61e827a66b376382a5
Reuben <grerlich@gmail.com>
2024-10-01 12:07:16 -0400

    added applications controller

---
+++ controllers/applications.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+const { models } = require('mongoose');
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const morgan = require('morgan');
 const session = require('express-session');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
+const applicationsController = require('./controllers/applications.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -34,11 +36,10 @@ app.get('/', (req, res) => {
     user: req.session.user,
   });
 });
-
-
-
 app.use('/auth', authController);
 app.use(isSignedIn);
+
+app.use('/users/:userId/applications', applicationsController);
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
