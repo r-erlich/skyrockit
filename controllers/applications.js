48eeb5ac04851c4553eaecdc10bd6e75d0b31dc5
Reuben <grerlich@gmail.com>
2024-10-01 14:01:34 -0400

    new application view

--- controllers/applications.js
+++ controllers/applications.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const User = require('../models/user.js');
 const { models } = require('mongoose');
 
+router.get('/', (req,res)=>{
+  try{
+      res.render('applications/index.ejs')
+  } catch {
+      console.error()
+  }
+})
 
-router.get('/', (req, res) => {
-    try {
-        res.render('applications/index.ejs');
-      } catch (error) {
-        console.log(error)
-        res.redirect('/')
-      }
-    });
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+  });
 module.exports = router;
