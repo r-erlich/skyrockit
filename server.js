fe696fb4f3072ea29482fbd91e41add89e5716ea
Reuben <grerlich@gmail.com>
2024-10-01 11:59:12 -0400

    updated middleware

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -27,7 +28,7 @@ app.use(
     saveUninitialized: true,
   })
 );
-
+app.use(passUserToView);
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -37,7 +38,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/auth', authController);
-
+app.use(isSignedIn);
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
